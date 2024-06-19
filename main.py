b70464edf003b568c301226df73ac5eafad10728
aym4n56 <127204323+aym4n56@users.noreply.github.com>
2024-06-19 12:49:24 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import flet as ft
+from views.FletRouter import Router
+
+def main(page: ft.Page):
+    myRouter = Router(page, ft)
+    page.on_route_change = myRouter.route_change
+
+    page.add(
+        myRouter.body
+    )
+
+ft.app(target=main)
---
+++ views/flashcard_content.py
@@ -0,0 +1,54 @@
+import sqlite3
+import os
+from flet import *
+from views import variables  # Import variables from the views folder
+
+directory = os.path.dirname(os.path.abspath(__file__))
+database_file_path = os.path.join(directory, 'flashcard.db')
+
+class FlashcardContent:
+    def __init__(self, page: Page):
+        self.page = page
+
+        self.conn = sqlite3.connect(database_file_path)
+        self.cursor = self.conn.cursor()
+
+        BG = '#041995'
+        FG = '#3450a1'
+
+        self.question_text_input = TextField(label='Question', width=300)
+        self.answer_text_input = TextField(label='Answer', width=300)
+        
+        def handle_submit_and_next(e):
+            self.submit_content(e)
+            page.go("/")  # Assuming this navigates back to the previous page
+
+        flashcard_content = Container(
+            content=Column(
+                controls=[
+                    ElevatedButton(text='Back', on_click=lambda _: page.go("/")),
+                    Container(height=20),
+                    Text(value='Enter your question and answer:', size=31, weight='bold'),
+                    Container(height=20),
+                    self.question_text_input,
+                    self.answer_text_input,
+                    ElevatedButton(text='Submit', on_click=self.submit_content),
+                    ElevatedButton(text='Done', on_click=handle_submit_and_next),
+                ],
+            ),
+        )
+
+        self.container = Container(
+            width=400,
+            height=850,
+            bgcolor=FG,
+            border_radius=35,
+            padding=padding.only(top=50, left=20, right=20, bottom=5),
+            content=flashcard_content,
+        )
+
+    def submit_content(self, e):
+        pass
+
+    def view(self):
+        return self.container
